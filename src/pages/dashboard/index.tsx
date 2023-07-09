ec8180310b43238d419a731e911d3e7c18c77b49
Abhishek-Kharpal <abhikharpal@gmail.com>
2023-07-09 23:37:56 +0530

    feat: add ui skeleton for dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import { Box, Typography, LinearProgress, Button, InputBase, IconButton, Paper } from '@mui/material';
+import { Box, Typography, LinearProgress, Button, InputBase, IconButton, Paper, Tooltip } from '@mui/material';
 import { Search } from '@mui/icons-material';
+import Card from '../../components/card';
 
 const Dashboard = () => {
   const [user, setUser] = useState(null);
@@ -10,6 +12,7 @@ const Dashboard = () => {
   useEffect(() => {
     // Get user details
     setUser({
+      id: '123',
       name: 'Abhishek',
       email: 'abk@gmail.com',
       storage: 400,
@@ -17,6 +20,78 @@ const Dashboard = () => {
       comments: [],
     });
     // Get files
+    setFiles([
+      {
+        id: '123',
+        url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+        name: 'Dummy',
+        author: 'Abhishek',
+        authorId: '123',
+        comments: [],
+        createdAt: new Date(),
+        size: 10,
+      },
+      {
+        id: '1',
+        url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+        name: 'Dummy',
+        author: 'Abhishek',
+        authorId: '123',
+        comments: [],
+        createdAt: new Date(),
+        size: 10,
+      },
+      {
+        id: '13',
+        url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+        name: 'Dummy',
+        author: 'Abhishek',
+        authorId: '123',
+        comments: [],
+        createdAt: new Date(),
+        size: 10,
+      },
+      {
+        id: '23',
+        url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+        name: 'Dummy',
+        author: 'Abhishek',
+        authorId: '123',
+        comments: [],
+        createdAt: new Date(),
+        size: 10,
+      },
+      {
+        id: '1023',
+        url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+        name: 'Dummy',
+        author: 'Abhishek',
+        authorId: '123',
+        comments: [],
+        createdAt: new Date(),
+        size: 10,
+      },
+      {
+        id: '1231',
+        url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+        name: 'Dummy',
+        author: 'Abhishek',
+        authorId: '123',
+        comments: [],
+        createdAt: new Date(),
+        size: 10,
+      },
+      {
+        id: '1232',
+        url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+        name: 'Dummy',
+        author: 'Abhishek',
+        authorId: '123',
+        comments: [],
+        createdAt: new Date(),
+        size: 10,
+      },
+    ]);
   }, []);
 
   return (
@@ -60,7 +135,7 @@ const Dashboard = () => {
             }}
           >
             <Typography variant="h4" color="primary.contrastText">
-              ShareIt
+              ShareIT
             </Typography>
           </Box>
 
@@ -171,27 +246,78 @@ const Dashboard = () => {
                 }}
                 placeholder="Search"
               />
-              <IconButton type="submit" sx={{ width: '20%' }} aria-label="search">
-                <Search color="secondary" />
-              </IconButton>
+              <Tooltip title="Search">
+                <IconButton type="submit" sx={{ width: '20%' }} aria-label="search">
+                  <Search color="secondary" />
+                </IconButton>
+              </Tooltip>
             </Paper>
           </Box>
           {/* Upload Card */}
           <Box
             sx={{
               display: 'flex',
-              flexDirection: 'row',
+              flexDirection: 'column',
               justifyContent: 'space-between',
-              alignItems: 'center',
             }}
             className="basic-padding"
           >
             <Typography variant="h6" color="primary.contrastText">
               Upload PDF
             </Typography>
+
+            <Box
+              sx={{
+                width: '128px',
+                height: '128px',
+                border: '1px dashed #F5F5F5',
+                marginTop: '8px',
+                cursor: 'pointer',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                '&:hover': {
+                  border: '1px solid #b785f5',
+                },
+              }}
+              className="basic-padding"
+            >
+              <Image src="/upload.svg" alt="Upload" width={50} height={50} />
+
+              <Typography variant="caption" color="primary.contrastText">
+                Upload your pdf here
+              </Typography>
+            </Box>
           </Box>
 
           {/* Your Files */}
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              flexGrow: 1,
+            }}
+            className="basic-padding"
+          >
+            <Typography variant="h6" color="primary.contrastText">
+              Your Files
+            </Typography>
+
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                marginTop: '8px',
+                flexWrap: 'wrap',
+              }}
+            >
+              {files.map((file) => (
+                <Card file={file} key={file.id} />
+              ))}
+            </Box>
+          </Box>
         </Box>
       </Box>
     </>
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,22 @@ export interface SnackbarI {
   severity: 'success' | 'info' | 'warning' | 'error';
   duration: number;
 }
+
+export interface FilesI {
+  id: string;
+  url: string;
+  name: string;
+  author: string;
+  authorId: string;
+  comments: CommentI[];
+  createdAt: Date;
+  size: number;
+}
+
+export interface CommentI {
+  id: string;
+  description: string;
+  author: string;
+  authorId: string;
+  createdAt: Date;
+}
